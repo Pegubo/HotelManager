797c2d6058832be8837130ea1a8eaf205a8431c8
Clarissa Araujo <clarissa.araujo@uabc.edu.mx>
2021-01-07 17:08:17 -0800

    historial depurado

--- app/src/main/java/com/example/hotelmanagerv1/HistorialActivity.java
+++ app/src/main/java/com/example/hotelmanagerv1/HistorialActivity.java
@@ -25,10 +25,12 @@ public class HistorialActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private ListView lista_pedidos;
     private FirebaseDatabase database;
-    private DatabaseReference ref;
+    private DatabaseReference Ref_pedidos;
+    private DatabaseReference Ref_servicios;
     private ArrayList<String> pedidos;
     private ArrayAdapter<String> adapter;
     private PedidosClass pedido;
+    private ServiciosClass servicioDisponible;
     private HabitacionesClass nHabitacion;
 
 
@@ -37,73 +39,50 @@ public class HistorialActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historial);
 
-        obtenerHabitacion();
         pedido=new PedidosClass();
         lista_pedidos=(ListView)findViewById(R.id.lista_pedidos);
         pedidos=new ArrayList<>();
         adapter=new ArrayAdapter<>(HistorialActivity.this,android.R.layout.simple_list_item_1,pedidos);
-        database=FirebaseDatabase.getInstance();
-        ref=database.getReference("pedidos");
 
 
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot postSnapShot : snapshot.getChildren()){
-                    pedido=postSnapShot.getValue(PedidosClass.class);
-                    if(pedido.getHabitacion()==nHabitacion.getNumero() && pedido.isCompletado()==true) {
-                        pedidos.add("Habitacion #" + Integer.toString(pedido.getHabitacion()) +
-                                " Almohadas: " + Integer.toString(pedido.getAlmohadas()) +
-                                " Toallas:   " + Integer.toString(pedido.getToallas()) +
-                                " Papel:     " + Integer.toString(pedido.getPapel()) +
-                                " Jabón      " + Integer.toString(pedido.getJabon()));
+        Ref_servicios = FirebaseDatabase.getInstance().getReference("servicios");
+        Ref_pedidos = FirebaseDatabase.getInstance().getReference("pedidos");
+
+        Bundle parametros = this.getIntent().getExtras();
+        if (parametros != null) {
+            servicioDisponible = (ServiciosClass) parametros.getSerializable("Servicio");
+            nHabitacion = (HabitacionesClass) parametros.getSerializable("habitacion");
+
+
+            Ref_pedidos.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot postSnapShot : snapshot.getChildren()) {
+                        pedido = postSnapShot.getValue(PedidosClass.class);
+                        if (pedido.getHabitacion() == nHabitacion.getNumero() && pedido.isCompletado() == true) {
+                            pedidos.add("Habitacion #" + Integer.toString(pedido.getHabitacion()) +
+                                    " Almohadas: " + Integer.toString(pedido.getAlmohadas()) +
+                                    " Toallas:   " + Integer.toString(pedido.getToallas()) +
+                                    " Papel:     " + Integer.toString(pedido.getPapel()) +
+                                    " Jabón      " + Integer.toString(pedido.getJabon()));
+                        }
                     }
+                    lista_pedidos.setAdapter(adapter);
+
                 }
-                lista_pedidos.setAdapter(adapter);
 
-            }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                }
+            });
 
-            }
-        });
+        }
+        else{
+            Toast.makeText(HistorialActivity.this, "No se han realizado pedidos a la habitacion #"+ nHabitacion.getNumero(), Toast.LENGTH_LONG).show();
 
+        }
 
     }
-
-    private void obtenerHabitacion(){
-        DatabaseReference mDatabaseRef;
-        ValueEventListener mDBListener;
-        List<HabitacionesClass> mHabitaciones;
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference("habitaciones");
-        mHabitaciones= new ArrayList<>();
-        mAuth = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        String email=currentUser.getEmail();
-        mDBListener = mDatabaseRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                for (DataSnapshot postSnapShot : snapshot.getChildren()){
-                    HabitacionesClass habitacion = postSnapShot.getValue(HabitacionesClass.class);
-                    habitacion.setmKey(postSnapShot.getKey());
-                    if(habitacion.getCorreo().equals(email)){
-                        nHabitacion=habitacion;
-                        Toast.makeText(HistorialActivity.this, "Habitacion Encontrada "+habitacion.getNumero(), Toast.LENGTH_SHORT).show();
-
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(HistorialActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-    }
-
-
 
 }
